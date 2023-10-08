988a50e61e4ee67658ce4f99cf1ccac642fcd445
viswa <saravanan.resume@gmail.com>
2023-10-09 01:45:21 +0800

    routing

---
+++ pages/blog/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+function Blog() {
+  return (
+    <>
+      <Link href='blog/first'>first</Link>
+      <p></p>
+      <Link href='blog/second'>second</Link>
+      <h1>Blog Page</h1>
+    </>
+  )
+}
+
+export default Blog
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 export default function Home() {
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log('Placing your order')
+    router.push('/product')
+  }
+
   return (
     <>
       <h1>Welcome Home</h1>
+      <p>
+        <Link href='/blog'>Blog</Link>
+      </p>
+      <p>
+        <Link href='/product'>Products</Link>
+      </p>
+      <p>
+        <Link href='/docs'>Docs</Link>
+      </p>
+      <p>
+        <Link href='/docs/any'>any</Link>
+      </p>
+      <button onClick={handleClick}>Place Order</button>
     </>
   )
 }
